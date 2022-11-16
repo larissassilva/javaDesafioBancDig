8c421d82362d7cc509e93e582c673e5bb2089cd4
Larissa Soares Silva <larissa.ssilva@gmail.com>
2022-11-16 18:14:45 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente Larissa = new Cliente();
+		Larissa.setNome("Larissa");
 		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.depositar(1000);
+		cc.transferir(1000, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
